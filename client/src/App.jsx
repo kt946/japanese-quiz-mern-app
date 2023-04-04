c003f44dcbefeff3fdea37ce0141ccaecaf7dc89
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-04 16:48:45 -0400

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <main>
+      <Home />
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-red-500">Hello World!</h1>
+    </div>
+  );
+};
+
+export default Home;
