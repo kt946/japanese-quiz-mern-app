395af90622971257e5d82a4283fb8d68ed064e0d
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-08 17:33:39 -0400

    updated Signup form and footer links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,6 +3,34 @@ import { Link } from 'react-router-dom';
 import { FaGithub, FaFacebook, FaDiscord, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
+  const socialLinks = [
+    {
+      name: 'Github',
+      url: 'https://github.com/',
+      icon: <FaGithub className="social-link" />,
+    },
+    {
+      name: 'Discord',
+      url: 'https://discord.com/',
+      icon: <FaDiscord className="social-link" />,
+    },
+    {
+      name: 'Facebook',
+      url: 'https://facebook.com/',
+      icon: <FaFacebook className="social-link" />,
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/',
+      icon: <FaTwitter className="social-link" />,
+    },
+    {
+      name: 'Instagram',
+      url: 'https://instagram.com/',
+      icon: <FaInstagram className="social-link" />,
+    },
+  ];
+
   return (
     <footer className="bg-[#2B2D42] text-slate-300 py-10">
       <div className="mx-auto max-w-7xl px-4 lg:px-8">
@@ -10,22 +38,15 @@ const Footer = () => {
           {/* Copyright */}
           <p className="text-sm text-center sm:text-left">Copyright © 2023 Kanji Camp. All rights reserved.</p>
           {/* Social Media Links */}
-          <div className="flex flex-row gap-4 order-first sm:order-last">
-            <Link to="https://github.com/">
-              <FaGithub className="w-8 h-8 btn-transition hover:text-white" />
-            </Link>
-            <Link to="https://discord.com/">
-              <FaDiscord className="w-8 h-8 btn-transition hover:text-white" />
-            </Link>
-            <Link to="https://www.facebook.com">
-              <FaFacebook className="w-8 h-8 btn-transition hover:text-white" />
-            </Link>
-            <Link to="https://twitter.com/">
-              <FaTwitter className="w-8 h-8 btn-transition hover:text-white" />
-            </Link>
-            <Link to="https://www.instagram.com/">
-              <FaInstagram className="w-8 h-8 btn-transition hover:text-white" />
-            </Link>
+          <div className="flex flex-row flex-wrap justify-center gap-4 order-first sm:order-last">
+            {socialLinks.map((link) => (
+              <Link
+                key={`link-${link.name}`}
+                to={link.url}
+              >
+                {link.icon}
+              </Link>
+            ))}
           </div>
         </div>
       </div>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,90 +1,107 @@
-import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/CampfireLogo.svg';
-
 import { HiEye, HiEyeOff } from 'react-icons/hi';
 
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    // Signup Form
     <section
       id="signup"
       className="w-full min-h-[calc(100vh-72px)] py-14 flex justify-center bg-slate-200 hero-bg"
     >
-      <div className="container flex justify-center">
-        <form className="w-full max-w-sm h-fit flex flex-col justify-center items-center bg-white p-8 rounded-xl shadow-xl">
-          <img
-            src={logo}
-            alt="Campfire Logo"
-            className="w-12 h-12 mx-auto mb-2"
-          />
-          <h1 className="text-2xl font-bold mb-6 text-center">Sign Up</h1>
-          {/* Fields Container */}
-          <div className="w-full flex flex-col gap-4">
-            {/* Name Field Wrapper*/}
-            <div className="flex flex-col gap-1">
-              <label
-                className="font-bold"
-                for="name"
-              >
-                Name
-              </label>
+      {/* Signup Form */}
+      <form className="w-full max-w-sm h-fit flex flex-col justify-center items-center bg-white p-8 rounded-xl shadow-xl">
+        <img
+          src={logo}
+          alt="Campfire Logo"
+          className="w-12 h-12 mx-auto mb-2"
+        />
+        <h1 className="text-2xl font-bold mb-6 text-center">Sign Up</h1>
+        {/* Fields Container */}
+        <div className="w-full flex flex-col gap-4">
+          {/* Username Field Wrapper*/}
+          <div className="flex flex-col gap-1">
+            <label
+              className="font-bold"
+              for="username"
+            >
+              Username
+            </label>
+            <input
+              className="px-3 py-2 bg-slate-100 border border-gray-300 rounded-xl focus:outline-none focus:border-primary"
+              type="text"
+              id="username"
+              name="username"
+              placeholder="John Doe"
+              required
+            />
+          </div>
+          {/* Email Field Wrapper*/}
+          <div className="flex flex-col gap-1">
+            <label
+              className="font-bold"
+              for="email"
+            >
+              Email
+            </label>
+            <input
+              className="px-3 py-2 bg-slate-100 border border-gray-300 rounded-xl focus:outline-none focus:border-primary"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="john@example.com"
+              required
+            />
+          </div>
+          {/* Password Field Wrapper */}
+          <div className="relative w-full flex flex-col gap-1">
+            <label
+              className="font-bold"
+              for="password"
+            >
+              Password
+            </label>
+            <div className="flex">
               <input
-                className="px-3 py-2 bg-slate-100 border border-gray-300 rounded-xl focus:outline-none focus:border-primary"
-                type="text"
-                id="name"
-                name="name"
-                placeholder="John Doe"
+                className="w-full px-3 py-2 pr-12 bg-slate-100 border border-gray-300 rounded-xl focus:outline-none focus:border-primary overflow-hidden"
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                name="password"
+                placeholder="********"
+                required
               />
-            </div>
-            {/* Email Field Wrapper*/}
-            <div className="flex flex-col gap-1">
-              <label
-                className="font-bold"
-                for="email"
+              {/* Show password button */}
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-0 btn-transition m-[1px] px-3 py-3 rounded-r-xl hover:bg-slate-200"
               >
-                Email
-              </label>
-              <input
-                className="px-3 py-2 bg-slate-100 border border-gray-300 rounded-xl focus:outline-none focus:border-primary"
-                type="email"
-                id="email"
-                name="email"
-                placeholder="john@example.com"
-              />
-            </div>
-            {/* Password Field Wrapper */}
-            <div className="relative w-full flex flex-col gap-1">
-              <label
-                className="font-bold"
-                for="password"
-              >
-                Password
-              </label>
-              <div className="flex">
-                <input
-                  className="w-full px-3 py-2 pr-12 bg-slate-100 border border-gray-300 rounded-xl focus:outline-none focus:border-primary overflow-hidden"
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="********"
-                />
-                {/* Show password button */}
-                <button className="absolute right-0 btn-transition m-[1px] px-3 py-3 rounded-r-xl hover:bg-slate-200">
-                  <HiEye />
-                </button>
-              </div>
+                {showPassword ? <HiEyeOff /> : <HiEye />}
+              </button>
             </div>
           </div>
+        </div>
 
-          {/* Submit Button */}
-          <button
-            className="w-full mt-6 py-3 px-6 bg-primary hover:bg-secondary btn-transition text-white font-bold rounded-xl "
-            type="submit"
+        {/* Submit Button */}
+        <button
+          className="w-full my-6 py-3 px-6 bg-primary hover:bg-secondary btn-transition text-white font-bold rounded-xl "
+          type="submit"
+        >
+          Create Account
+        </button>
+        {/* Login Link */}
+        <p className="text-[#4D516D] text-center">
+          Already have an account?{' '}
+          <Link
+            to="/login"
+            className="font-bold text-primary hover:text-secondary btn-transition hover:underline"
           >
-            Create Account
-          </button>
-        </form>
-      </div>
+            Login
+          </Link>
+        </p>
+      </form>
     </section>
   );
 };
