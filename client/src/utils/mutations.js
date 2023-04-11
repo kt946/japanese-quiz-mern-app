36cda6c88835724e9ae179c0c7e4e705c8c611ea
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-11 15:56:18 -0400

    added mutations to utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,36 @@
+// This file stores all of the GraphQL mutation requests.
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const UPDATE_EXPERIENCE = gql`
+  mutation updateExperience($experience: Int!) {
+    updateExperience(experience: $experience) {
+      _id
+      username
+      experience
+    }
+  }
+`;
