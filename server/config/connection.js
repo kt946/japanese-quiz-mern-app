bce93b437405cbf5b1820bea5332ef7f761dd102
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-11 14:55:25 -0400

    added MongoDB connection file

---
+++ server/config/connection.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'; // import mongoose for MongoDB connection
+
+// set up connection to MongoDB
+const connectDB = async (url) => {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected!');
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export default connectDB;
