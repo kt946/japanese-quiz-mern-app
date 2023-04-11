270620af0258ad6ec73837c470e2b229e446dc07
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-11 15:28:45 -0400

    added express server functionality

---
+++ server/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express'; // import ApolloServer for GraphQL
+import path from 'path';
+import * as dotenv from 'dotenv';
+
+import connectDB from './config/connection.js'; // import connection to MongoDB
+import { typeDefs, resolvers } from './schemas/index.js'; // import typeDefs and resolvers
+import { authMiddleware } from './utils/auth.js'; // import authMiddleware for authentication
+
+dotenv.config(); // Load environment variables from .env file
+
+const PORT = process.env.PORT || 3001;
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware, // This ensures that every request performs an authentication check, and the updated request object will be passed to the resolvers as the context.
+});
+
+const app = express(); // Create a new instance of an Express server
+app.use(express.urlencoded({ extended: true })); // This sets up middleware to parse incoming requests with urlencoded payloads
+app.use(express.json()); // This sets up middleware to parse incoming requests with JSON payloads
+
+// This will serve up static assets from the client/dist folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+}
+
+// This is a catch-all route that sends every other request to the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+const startServer = async (typeDefs, resolvers) => {
+  try {
+    await server.start(); // Start the Apollo server
+    server.applyMiddleware({ app }); // integrate our Apollo server with the Express application as middleware
+    connectDB(process.env.MONGODB_URI); // connect to MongoDB
+    app.listen(3001, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer(typeDefs, resolvers); // Start server
