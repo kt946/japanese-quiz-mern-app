cbb377bc16b1df1717d492213774eb44d1f5eda6
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-11 16:06:05 -0400

    updated App.jsx with Apollo Provider and added server proxy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,59 @@
 import { Header, Footer } from './components';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import { Home, Login, Signup } from './pages';
+
+// This will connect to the server's GraphQL endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// This will set the authorization header for the request
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// This will instantiate the Apollo Client instance and create the connection to the API endpoint
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app transition-all ease-in text-slate-800">
-        <Header />
-        <main>
-          <Routes>
-            <Route
-              path="/"
-              element={<Home />}
-            />
-            <Route
-              path="/login"
-              element={<Login />}
-            />
-            <Route
-              path="/signup"
-              element={<Signup />}
-            />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    // This will make the client available to all components
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <div className="app transition-all ease-in text-slate-800">
+          <Header />
+          <main>
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/graphql': 'http://localhost:3001/graphql',
+    },
+  },
   plugins: [react()],
-})
+});
