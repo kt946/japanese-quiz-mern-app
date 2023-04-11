9b6baac898892e2ff73b1f98a2c4bb6e921de743
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-11 15:14:05 -0400

    added typeDefs to schemas

---
+++ server/schemas/index.js
@@ -0,0 +1,4 @@
+import typeDefs from './typeDefs.js';
+import resolvers from './resolvers.js';
+
+export { typeDefs, resolvers };
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,32 @@
+// import the gql tagged template function
+import { gql } from 'apollo-server-express';
+
+// create our typeDefs
+// all type definitions need to specify the type of data that is returning.
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    experience: Int
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    updateExperience(experience: Int!): User
+  }
+`;
+
+export default typeDefs;
