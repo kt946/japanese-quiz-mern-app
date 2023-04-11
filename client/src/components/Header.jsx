18979ea0449ea0aef04ff7ba6c6eb5f1d8416fda
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-11 16:20:55 -0400

    updated Header to render logout button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 import logo from '../assets/SakuraStudyLogo.svg';
 import logoText from '../assets/SakuraStudyTextBlack.svg';
 
 const Header = () => {
+  // check the logged in status of a user; returns true if logged in
+  const loggedIn = Auth.loggedIn();
+  
   return (
     <header className="bg-white z-50">
       <nav className="mx-auto max-w-7xl flex items-center justify-between p-4 lg:px-8">
@@ -23,21 +27,32 @@ const Header = () => {
             className="w-auto h-6 hidden sm:inline-block"
           />
         </Link>
-        {/* Login and Sign Up Links */}
-        <div className="flex items-center gap-4 font-bold">
-          <Link
-            to="/login"
-            className="btn-transition py-2 px-4 hover:text-primary-shade"
+        {/* Renders logout button if logged in */}
+        {loggedIn ? (
+          // Logout button
+          <button
+            onClick={() => Auth.logout()}
+            className="btn-transition py-2 px-4 text-white bg-primary hover:bg-primary-shade rounded-xl font-bold"
           >
-            Log in
-          </Link>
-          <Link
-            to="/signup"
-            className="btn-transition py-2 px-4 text-white bg-primary hover:bg-primary-shade rounded-xl"
-          >
-            Sign up
-          </Link>
-        </div>
+            Log out
+          </button>
+        ) : (
+          // Login and Sign Up Links
+          <div className="flex items-center gap-4 font-bold">
+            <Link
+              to="/login"
+              className="btn-transition py-2 px-4 hover:text-primary-shade"
+            >
+              Log in
+            </Link>
+            <Link
+              to="/signup"
+              className="btn-transition py-2 px-4 text-white bg-primary hover:bg-primary-shade rounded-xl"
+            >
+              Sign up
+            </Link>
+          </div>
+        )}
       </nav>
     </header>
   );
