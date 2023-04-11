65bab59cd85c1c1953afe878afbde0eb5fd22a4c
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-11 15:01:37 -0400

    added User model

---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+import { Schema, model } from 'mongoose'; // import Schema and model from mongoose
+import bcrypt from 'bcrypt'; // import bcrypt for password hashing
+
+// create User schema with username, email, password, and experience fields
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    // match option is a type of validation that allows us to use regex to test the input value.
+    // If it doesn't match, we send back a custom error message stating that there must be a valid email address.
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5, // password must be at least 5 characters long
+  },
+  experience: {
+    type: Number,
+    default: 0,
+  },
+});
+
+// Hash the password before saving it to the database
+userSchema.pre('save', async function (next) {
+  // if the password has not been modified, then we don't need to hash it again
+  // if the password has been modified, then we need to hash it again
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// Compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema); // create User model using the User schema
+
+export default User; // export the User model
---
+++ server/models/index.js
@@ -0,0 +1,3 @@
+import User from './User.js';
+
+export default User;
