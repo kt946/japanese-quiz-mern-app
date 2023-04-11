781fd6ddcfac3d119c303ad828aa1c6b72724de5
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-11 16:01:34 -0400

    added queries to utils

---
+++ client/src/utils/queries.js
@@ -0,0 +1,36 @@
+// This file stores all of the GraphQL query requests.
+import { gql } from '@apollo/client';
+
+// Because we aren't passing any variables to it, we can simply name the query, and GraphQL will handle the rest.
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      experience
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      experience
+    }
+  }
+`;
+
+export const QUERY_USERS = gql`
+  {
+    users {
+      _id
+      username
+      email
+      experience
+    }
+  }
+`;
