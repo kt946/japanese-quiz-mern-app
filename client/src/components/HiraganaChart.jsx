f623cc163d0cc7623adafb2a174ecddc99dd180a
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-15 14:28:21 -0400

    updated hiragana chart with additional syllables

--- client/src/components/HiraganaChart.jsx
+++ client/src/components/HiraganaChart.jsx
@@ -3,10 +3,33 @@ import { hiraganaData } from '../data';
 
 const HiraganaChart = () => {
   return (
+    // Hiragana Chart Section
     <section id="hiraganaChart">
-      <h3 className="font-bold mb-2 text-lg">Hiragana Chart</h3>
-      <div className="grid grid-cols-5 gap-4 text-4xl text-center font-bold">
+      <h3 className="font-bold mb-2 text-2xl">Hiragana Chart</h3>
+
+      {/* Basic Hiragana Container */}
+      <div className="chart-container">
         {hiraganaData.basic.map((char) => (
+          // Character Container
+          <div
+            key={`id-${char.romaji}`}
+            className={`p-2 flex flex-col rounded-xl shadow-md ${
+              char.romaji ? 'bg-red-100/50 border-2 border-primary-shade/40' : 'bg-slate-100'
+            }`}
+          >
+            {/* Character */}
+            {char.character}
+            {/* Romaji */}
+            <span className="chart-romaji">{char.romaji}</span>
+          </div>
+        ))}
+      </div>
+
+      <hr className="my-8 border-2 border-slate-200" />
+
+      {/* Dakuon/Diacritical Marks Container */}
+      <div className="chart-container">
+        {hiraganaData.dakuon.map((char) => (
           <div
             key={`id-${char.romaji}`}
             className={`p-2 flex flex-col rounded-xl shadow-md ${
@@ -14,8 +37,7 @@ const HiraganaChart = () => {
             }`}
           >
             {char.character}
-
-            <span className="font-normal text-xl">{char.romaji}</span>
+            <span className="chart-romaji">{char.romaji}</span>
           </div>
         ))}
       </div>
