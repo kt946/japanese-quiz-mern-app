583bdffc796226cdd57e650606a7809a26316885
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-10-08 23:16:02 -0400

    Updated social links

--- client/src/constants/constants.jsx
+++ client/src/constants/constants.jsx
@@ -1,7 +1,7 @@
 import { HiOutlineHome, HiOutlineUserCircle } from 'react-icons/hi';
 import { IoLanguage } from 'react-icons/io5';
 import { MdOutlineLeaderboard } from 'react-icons/md';
-import { FaGithub, FaFacebook, FaDiscord, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 
 const sidebarNavItems = [
   {
@@ -26,32 +26,11 @@ const sidebarNavItems = [
   },
 ];
 
-// All links are placeholders except for Github
 const socialLinks = [
   {
     title: 'Github',
     url: 'https://github.com/kt946/japanese-quiz-mern-app',
     icon: <FaGithub className="social-link" />,
-  },
-  {
-    title: 'Discord',
-    url: 'https://discord.com/',
-    icon: <FaDiscord className="social-link" />,
-  },
-  {
-    title: 'Facebook',
-    url: 'https://facebook.com/',
-    icon: <FaFacebook className="social-link" />,
-  },
-  {
-    title: 'Twitter',
-    url: 'https://twitter.com/',
-    icon: <FaTwitter className="social-link" />,
-  },
-  {
-    title: 'Instagram',
-    url: 'https://instagram.com/',
-    icon: <FaInstagram className="social-link" />,
   },
 ];
 
