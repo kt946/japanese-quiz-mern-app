7173ec5f52d4d065d932fd00d13452db63482fc8
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-10-10 15:19:36 -0400

    Added lesson 2 vocabulary

--- client/src/data/lessons/index.js
+++ client/src/data/lessons/index.js
@@ -1,6 +1,7 @@
 import lesson_0_data from './lesson-0/lesson-0-data';
 import lesson_1_data from './lesson-1/lesson-1-data';
+import lesson_2_data from './lesson-2/lesson-2-data';
 
-const lessonData = [lesson_0_data, lesson_1_data];
+const lessonData = [lesson_0_data, lesson_1_data, lesson_2_data];
 
 export default lessonData;
---
+++ client/src/data/lessons/lesson-2/lesson-2-data.js
@@ -0,0 +1,244 @@
+const lesson_2_data = {
+  lessonNumber: 2,
+  lessonTitle: 'Shopping',
+  lessonUrl: 'lesson-2',
+  lessonPages: 'Genki I: p.56-81',
+  lessonUnits: [
+    {
+      unitTitle: 'Vocabulary',
+      unitUrl: 'vocabulary',
+      unitContent: [
+        {
+          character: 'これ',
+          meanings: 'this one',
+        },
+        {
+          character: 'それ',
+          meanings: 'that one',
+        },
+        {
+          character: 'あれ',
+          meanings: 'that one (over there)',
+        },
+        {
+          character: 'どれ',
+          meanings: 'which one',
+        },
+        {
+          character: 'この',
+          meanings: 'this ...',
+        },
+        {
+          character: 'その',
+          meanings: 'that ...',
+        },
+        {
+          character: 'あの',
+          meanings: 'that ... (over there)',
+        },
+        {
+          character: 'どの',
+          meanings: 'which ...',
+        },
+        {
+          character: 'ここ',
+          meanings: 'here',
+        },
+        {
+          character: 'そこ',
+          meanings: 'there',
+        },
+        {
+          character: 'あそこ',
+          meanings: 'over there',
+        },
+        {
+          character: 'どこ',
+          meanings: 'where',
+        },
+        {
+          character: 'だれ',
+          meanings: 'who',
+        },
+        {
+          character: 'おいしい',
+          meanings: 'delicious',
+        },
+        {
+          character: 'さかな',
+          meanings: 'fish',
+        },
+        {
+          character: 'とんかつ',
+          meanings: 'pork cutlet',
+        },
+        {
+          character: 'にく',
+          meanings: 'meat',
+        },
+        {
+          character: 'メニュー',
+          meanings: 'menu',
+        },
+        {
+          character: 'やさい',
+          meanings: 'vegetable',
+        },
+        {
+          character: 'かさ',
+          meanings: 'umbrella',
+        },
+        {
+          character: 'かばん',
+          meanings: 'bag',
+        },
+        {
+          character: 'くつ',
+          meanings: 'shoes',
+        },
+        {
+          character: 'さいふ',
+          meanings: 'wallet',
+        },
+        {
+          character: 'ジーンズ',
+          meanings: 'jeans',
+        },
+        {
+          character: 'じてんしゃ',
+          meanings: 'bicycle',
+        },
+        {
+          character: 'しんぶん',
+          meanings: 'newspaper',
+        },
+        {
+          character: 'スマホ',
+          meanings: 'smartphone; mobile',
+        },
+        {
+          character: 'Ｔシャツ',
+          meanings: 'T-shirt',
+        },
+        {
+          character: 'とけい',
+          meanings: 'watch; clock',
+        },
+        {
+          character: 'ノート',
+          meanings: 'notebook',
+        },
+        {
+          character: 'ペン',
+          meanings: 'pen',
+        },
+        {
+          character: 'ぼうし',
+          meanings: 'hat; cap',
+        },
+        {
+          character: 'ほん',
+          meanings: 'book',
+        },
+        {
+          character: 'ぎんこう',
+          meanings: 'bank',
+        },
+        {
+          character: 'コンビニ',
+          meanings: 'convenience store',
+        },
+        {
+          character: 'トイレ',
+          meanings: 'toilet; restroom',
+        },
+        {
+          character: 'としょかん',
+          meanings: 'library',
+        },
+        {
+          character: 'ゆうびんきょく',
+          meanings: 'post office',
+        },
+        {
+          character: 'イギリス',
+          meanings: 'Britain',
+        },
+        {
+          character: 'かんこく',
+          meanings: 'Korea',
+        },
+        {
+          character: 'ちゅうごく',
+          meanings: 'China',
+        },
+        {
+          character: 'えいご',
+          meanings: 'English (language)',
+        },
+        {
+          character: 'けいざい',
+          meanings: 'economics',
+        },
+        {
+          character: 'コンピューター',
+          meanings: 'computer',
+        },
+        {
+          character: 'ビジネス',
+          meanings: 'business',
+        },
+        {
+          character: 'れきし',
+          meanings: 'history',
+        },
+        {
+          character: 'おかあさん',
+          meanings: 'mother',
+        },
+        {
+          character: 'おとうさん',
+          meanings: 'father',
+        },
+        {
+          character: 'いくら',
+          meanings: 'how much',
+        },
+        {
+          character: '～えん',
+          meanings: '... yen',
+        },
+        {
+          character: 'たかい',
+          meanings: 'expensive; high',
+        },
+        {
+          character: 'いらっしゃいませ',
+          meanings: 'Welcome (to our store).',
+        },
+        {
+          character: '(～を) おねがいします',
+          meanings: '..., please.',
+        },
+        {
+          character: '(～を)ください',
+          meanings: 'Please give me ...',
+        },
+        {
+          character: 'じゃあ',
+          meanings: 'then ...; if that is the case, ...',
+        },
+        {
+          character: 'どうぞ',
+          meanings: 'Please.; Here it is.',
+        },
+        {
+          character: 'どうも',
+          meanings: 'Thank you.',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_2_data;
