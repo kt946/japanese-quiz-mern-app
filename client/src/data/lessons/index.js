0cdd1a46b5a0b1a62eed51d14ebeaaa5102a8c3d
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-10-11 16:38:30 -0400

    Added lesson 4 vocabulary exercise

--- client/src/data/lessons/index.js
+++ client/src/data/lessons/index.js
@@ -2,7 +2,8 @@ import lesson_0_data from './lesson-0/lesson-0-data';
 import lesson_1_data from './lesson-1/lesson-1-data';
 import lesson_2_data from './lesson-2/lesson-2-data';
 import lesson_3_data from './lesson-3/lesson-3-data';
+import lesson_4_data from './lesson-4/lesson-4-data';
 
-const lessonData = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data];
+const lessonData = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data, lesson_4_data];
 
 export default lessonData;
---
+++ client/src/data/lessons/lesson-4/lesson-4-data.js
@@ -0,0 +1,264 @@
+const lesson_4_data = {
+  lessonNumber: 4,
+  lessonTitle: 'The First Date',
+  lessonUrl: 'lesson-4',
+  lessonPages: 'Genki I: p.102-127',
+  lessonUnits: [
+    {
+      unitTitle: 'Vocabulary',
+      unitUrl: 'vocabulary',
+      unitContent: [
+        {
+          character: 'ゲーム',
+          meanings: 'game',
+        },
+        {
+          character: 'アルバイト',
+          meanings: 'part-time job',
+        },
+        {
+          character: 'かいもの',
+          meanings: 'shopping',
+        },
+        {
+          character: 'クラス',
+          meanings: 'class',
+        },
+        {
+          character: 'いぬ',
+          meanings: 'dog',
+        },
+        {
+          character: 'ねこ',
+          meanings: 'cat',
+        },
+        {
+          character: 'ひと',
+          meanings: 'person',
+        },
+        {
+          character: 'こども',
+          meanings: 'child',
+        },
+        {
+          character: 'あなた',
+          meanings: 'you',
+        },
+        {
+          character: 'いす',
+          meanings: 'chair',
+        },
+        {
+          character: 'つくえ',
+          meanings: 'desk',
+        },
+        {
+          character: 'しゃしん',
+          meanings: 'picture; photograph',
+        },
+        {
+          character: 'はな',
+          meanings: 'flower',
+        },
+        {
+          character: 'レポート',
+          meanings: '(term) paper',
+        },
+        {
+          character: 'ごはん',
+          meanings: 'rice; meal',
+        },
+        {
+          character: 'パン',
+          meanings: 'bread',
+        },
+        {
+          character: 'おてら',
+          meanings: 'temple',
+        },
+        {
+          character: 'こうえん',
+          meanings: 'park',
+        },
+        {
+          character: 'スーパー',
+          meanings: 'supermarket',
+        },
+        {
+          character: 'バスてい',
+          meanings: 'bus stop',
+        },
+        {
+          character: 'びょういん',
+          meanings: 'hospital',
+        },
+        {
+          character: 'ホテル',
+          meanings: 'hotel',
+        },
+        {
+          character: 'ほんや',
+          meanings: 'bookstore',
+        },
+        {
+          character: 'まち',
+          meanings: 'town; city',
+        },
+        {
+          character: 'レストラン',
+          meanings: 'restaurant',
+        },
+        {
+          character: 'きのう',
+          meanings: 'yesterday',
+        },
+        {
+          character: '～じかん',
+          meanings: '... hours',
+        },
+        {
+          character: 'いちじかん',
+          meanings: 'one hour',
+        },
+        {
+          character: 'せんしゅう',
+          meanings: 'last week',
+        },
+        {
+          character: 'とき',
+          meanings: 'when ...; at the time of ...',
+        },
+        {
+          character: 'げつようび',
+          meanings: 'Monday',
+        },
+        {
+          character: 'かようび',
+          meanings: 'Tuesday',
+        },
+        {
+          character: 'すいようび',
+          meanings: 'Wednesday',
+        },
+        {
+          character: 'もくようび',
+          meanings: 'Thursday',
+        },
+        {
+          character: 'きんようび',
+          meanings: 'Friday',
+        },
+        {
+          character: 'あう',
+          meanings: 'to meet; to see (a person)',
+        },
+        {
+          character: 'ある',
+          meanings: 'there is ...',
+        },
+        {
+          character: 'かう',
+          meanings: 'to buy',
+        },
+        {
+          character: 'かく',
+          meanings: 'to write',
+        },
+        {
+          character: 'とる',
+          meanings: 'to take (a picture)',
+        },
+        {
+          character: 'まつ',
+          meanings: 'to wait',
+        },
+        {
+          character: 'わかる',
+          meanings: 'to understand',
+        },
+        {
+          character: 'いる',
+          meanings: '(a person) is in .. ; stays at ...',
+        },
+        {
+          character: '～ぐらい',
+          meanings: 'about (approximate measurement)',
+        },
+        {
+          character: 'ごめんなさい',
+          meanings: "I'm sorry.",
+        },
+        {
+          character: 'それから',
+          meanings: 'and then',
+        },
+        {
+          character: 'だから',
+          meanings: 'so; therefore',
+        },
+        {
+          character: 'たくさん',
+          meanings: 'many; a lot',
+        },
+        {
+          character: '～と',
+          meanings: 'together with (a person); and',
+        },
+        {
+          character: 'どうして',
+          meanings: 'why',
+        },
+        {
+          character: 'ひとりで',
+          meanings: 'alone',
+        },
+        {
+          character: 'もしもし',
+          meanings: 'Hello? (used on the phone)',
+        },
+        {
+          character: 'みぎ',
+          meanings: 'right (～の)',
+        },
+        {
+          character: 'ひだり',
+          meanings: 'left (～の)',
+        },
+        {
+          character: 'まえ',
+          meanings: 'front (～の)',
+        },
+        {
+          character: 'うしろ',
+          meanings: 'back (～の)',
+        },
+        {
+          character: 'なか',
+          meanings: 'inside (～の)',
+        },
+        {
+          character: 'うえ',
+          meanings: 'on (～の)',
+        },
+        {
+          character: 'した',
+          meanings: 'under (～の)',
+        },
+        {
+          character: 'ちかく',
+          meanings: 'near; nearby (～の)',
+        },
+        {
+          character: 'となり',
+          meanings: 'next (～の)',
+        },
+        {
+          character: 'あいだ',
+          meanings: 'between (A と B の)',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_4_data;
