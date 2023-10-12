fd0fb5462ba7e34a58cb591e1bc47e909a3ec931
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-10-12 16:16:38 -0400

    Added lesson 8 vocabulary exercise

--- client/src/data/lessons/index.js
+++ client/src/data/lessons/index.js
@@ -6,7 +6,18 @@ import lesson_4_data from './lesson-4/lesson-4-data';
 import lesson_5_data from './lesson-5/lesson-5-data';
 import lesson_6_data from './lesson-6/lesson-6-data';
 import lesson_7_data from './lesson-7/lesson-7-data';
+import lesson_8_data from './lesson-8/lesson-8-data';
 
-const lessonData = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data, lesson_4_data, lesson_5_data, lesson_6_data, lesson_7_data];
+const lessonData = [
+  lesson_0_data,
+  lesson_1_data,
+  lesson_2_data,
+  lesson_3_data,
+  lesson_4_data,
+  lesson_5_data,
+  lesson_6_data,
+  lesson_7_data,
+  lesson_8_data,
+];
 
 export default lessonData;
---
+++ client/src/data/lessons/lesson-8/lesson-8-data.js
@@ -0,0 +1,236 @@
+const lesson_8_data = {
+  lessonNumber: 8,
+  lessonTitle: 'Barbecue',
+  lessonUrl: 'lesson-8',
+  lessonPages: 'Genki I: p.186-209',
+  lessonUnits: [
+    {
+      unitTitle: 'Vocabulary',
+      unitUrl: 'vocabulary',
+      unitContent: [
+        {
+          character: 'はれ',
+          meanings: 'sunny weather',
+        },
+        {
+          character: 'あめ',
+          meanings: 'rain',
+        },
+        {
+          character: 'くもり',
+          meanings: 'cloudy weather',
+        },
+        {
+          character: 'ゆき',
+          meanings: 'snow',
+        },
+        {
+          character: 'てんきよほう',
+          meanings: 'weather forecast',
+        },
+        {
+          character: 'きおん',
+          meanings: 'temperature (weather)',
+        },
+        {
+          character: 'なつ',
+          meanings: 'summer',
+        },
+        {
+          character: 'ふゆ',
+          meanings: 'winter',
+        },
+        {
+          character: 'けさ',
+          meanings: 'this morning',
+        },
+        {
+          character: 'あさって',
+          meanings: 'the day after tomorrow',
+        },
+        {
+          character: 'まいしゅう',
+          meanings: 'every week',
+        },
+        {
+          character: 'こんげつ',
+          meanings: 'this month',
+        },
+        {
+          character: 'らいげつ',
+          meanings: 'next month',
+        },
+        {
+          character: 'かいしゃいん',
+          meanings: 'office worker',
+        },
+        {
+          character: 'しごと',
+          meanings: 'job; work; occupation',
+        },
+        {
+          character: 'カメラ',
+          meanings: 'camera',
+        },
+        {
+          character: 'カラオケ',
+          meanings: 'karaoke',
+        },
+        {
+          character: 'ところ',
+          meanings: 'place',
+        },
+        {
+          character: 'トマト',
+          meanings: 'tomato',
+        },
+        {
+          character: 'はし',
+          meanings: 'chopsticks',
+        },
+        {
+          character: 'パーティー',
+          meanings: 'party',
+        },
+        {
+          character: 'バーベキュー',
+          meanings: 'barbecue',
+        },
+        {
+          character: 'ホームステイ',
+          meanings: 'homestay; living with a local family',
+        },
+        {
+          character: 'おふろ',
+          meanings: 'bath',
+        },
+        {
+          character: 'スペイン',
+          meanings: 'Spain',
+        },
+        {
+          character: 'なにか',
+          meanings: 'something',
+        },
+        {
+          character: 'じょうず（な）',
+          meanings: 'skillful; good at ...',
+        },
+        {
+          character: 'へた（な）',
+          meanings: 'clumsy; poor at ...',
+        },
+        {
+          character: 'ゆうめい（な）',
+          meanings: 'famous',
+        },
+        {
+          character: 'あらう',
+          meanings: 'to wash',
+        },
+        {
+          character: 'いう',
+          meanings: 'to say',
+        },
+        {
+          character: 'いる',
+          meanings: 'to need',
+        },
+        {
+          character: 'おそくなる',
+          meanings: 'to be late',
+        },
+        {
+          character: 'おふろにはいる',
+          meanings: 'to take a bath',
+        },
+        {
+          character: 'おもう',
+          meanings: 'to think',
+        },
+        {
+          character: 'きる',
+          meanings: 'to cut',
+        },
+        {
+          character: 'つくる',
+          meanings: 'to make',
+        },
+        {
+          character: '（あめ／ゆきが）ふる',
+          meanings: '(rain/snow) falls',
+        },
+        {
+          character: 'もっていく',
+          meanings: 'to take (a thing)',
+        },
+        {
+          character: 'すてる',
+          meanings: 'to throw away',
+        },
+        {
+          character: 'はじめる',
+          meanings: 'to begin',
+        },
+        {
+          character: 'うんてんする',
+          meanings: 'to drive',
+        },
+        {
+          character: 'せんたくする',
+          meanings: 'to do laundry',
+        },
+        {
+          character: 'そうじする',
+          meanings: 'to clean',
+        },
+        {
+          character: 'りょうりする',
+          meanings: 'to cook',
+        },
+        {
+          character: 'うん',
+          meanings: 'uh-huh; yes',
+        },
+        {
+          character: 'ううん',
+          meanings: 'uh-uh; no',
+        },
+        {
+          character: 'いつも',
+          meanings: 'always',
+        },
+        {
+          character: 'おそく',
+          meanings: '(do something) late',
+        },
+        {
+          character: 'かんぱい',
+          meanings: 'Cheers! (a toast)',
+        },
+        {
+          character: 'みんなで',
+          meanings: 'all (of the people) together',
+        },
+        {
+          character: 'ざんねん（ですね）',
+          meanings: "That's too bad.",
+        },
+        {
+          character: 'まだ + negative',
+          meanings: 'not ... yet',
+        },
+        {
+          character: '～ど',
+          meanings: '... degrees (temperature)',
+        },
+        {
+          character: 'どう',
+          meanings: 'how',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_8_data;
