6c333dfa3a0ff1c13be3c8680820ecaf8ab58ef6
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-10-12 14:34:47 -0400

    Added lesson 6 vocabulary exercise

--- client/src/data/lessons/index.js
+++ client/src/data/lessons/index.js
@@ -4,7 +4,8 @@ import lesson_2_data from './lesson-2/lesson-2-data';
 import lesson_3_data from './lesson-3/lesson-3-data';
 import lesson_4_data from './lesson-4/lesson-4-data';
 import lesson_5_data from './lesson-5/lesson-5-data';
+import lesson_6_data from './lesson-6/lesson-6-data';
 
-const lessonData = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data, lesson_4_data, lesson_5_data];
+const lessonData = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data, lesson_4_data, lesson_5_data, lesson_6_data];
 
 export default lessonData;
---
+++ client/src/data/lessons/lesson-6/lesson-6-data.js
@@ -0,0 +1,204 @@
+const lesson_6_data = {
+  lessonNumber: 6,
+  lessonTitle: "A Day in Robert's Life",
+  lessonUrl: 'lesson-6',
+  lessonPages: 'Genki I: p.146-165',
+  lessonUnits: [
+    {
+      unitTitle: 'Vocabulary',
+      unitUrl: 'vocabulary',
+      unitContent: [
+        {
+          character: 'かんじ',
+          meanings: 'kanji; Chinese character',
+        },
+        {
+          character: 'きょうかしょ',
+          meanings: 'textbook',
+        },
+        {
+          character: 'ページ',
+          meanings: 'page',
+        },
+        {
+          character: 'つぎ',
+          meanings: 'next',
+        },
+        {
+          character: 'おかね',
+          meanings: 'money',
+        },
+        {
+          character: 'にもつ',
+          meanings: 'baggage',
+        },
+        {
+          character: 'パソコン',
+          meanings: 'personal computer',
+        },
+        {
+          character: 'シャワー',
+          meanings: 'shower',
+        },
+        {
+          character: 'エアコン',
+          meanings: 'air conditioner',
+        },
+        {
+          character: 'でんき',
+          meanings: 'electricity; light',
+        },
+        {
+          character: 'まど',
+          meanings: 'window',
+        },
+        {
+          character: 'でんしゃ',
+          meanings: 'train',
+        },
+        {
+          character: 'くに',
+          meanings: 'country; place of origin',
+        },
+        {
+          character: 'こんしゅう',
+          meanings: 'this week',
+        },
+        {
+          character: 'らいしゅう',
+          meanings: 'next week',
+        },
+        {
+          character: 'らいねん',
+          meanings: 'next year',
+        },
+        {
+          character: 'よる',
+          meanings: 'night',
+        },
+        {
+          character: 'たいへん（な）',
+          meanings: 'tough (situation)',
+        },
+        {
+          character: 'あそぶ',
+          meanings: 'to play; to spend time pleasantly',
+        },
+        {
+          character: 'いそぐ',
+          meanings: 'to hurry',
+        },
+        {
+          character: 'かえす',
+          meanings: 'to return (a thing)',
+        },
+        {
+          character: 'けす',
+          meanings: 'to turn off; to erase',
+        },
+        {
+          character: 'しぬ',
+          meanings: 'to die',
+        },
+        {
+          character: 'すわる',
+          meanings: 'to sit down',
+        },
+        {
+          character: 'たつ',
+          meanings: 'to stand up',
+        },
+        {
+          character: 'たばこをすう',
+          meanings: 'to smoke',
+        },
+        {
+          character: 'つかう',
+          meanings: 'to use',
+        },
+        {
+          character: 'てつだう',
+          meanings: 'to help',
+        },
+        {
+          character: 'はいる',
+          meanings: 'to enter',
+        },
+        {
+          character: 'もつ',
+          meanings: 'to carry; to hold',
+        },
+        {
+          character: 'やすむ',
+          meanings: 'to be absent (from ... ); to rest',
+        },
+        {
+          character: 'あける',
+          meanings: 'to open (something)',
+        },
+        {
+          character: 'しめる',
+          meanings: 'to close (something)',
+        },
+        {
+          character: 'おしえる',
+          meanings: 'to teach; to instruct',
+        },
+        {
+          character: 'わすれる',
+          meanings: 'to forget; to leave behind',
+        },
+        {
+          character: 'おりる',
+          meanings: 'to get off',
+        },
+        {
+          character: 'かりる',
+          meanings: 'to borrow',
+        },
+        {
+          character: 'シャワーをあびる',
+          meanings: 'to take a shower',
+        },
+        {
+          character: 'つける',
+          meanings: 'to turn on',
+        },
+        {
+          character: 'でんわする',
+          meanings: 'to call',
+        },
+        {
+          character: 'つれてくる',
+          meanings: 'to bring (a person)',
+        },
+        {
+          character: 'もってくる',
+          meanings: 'to bring (a thing)',
+        },
+        {
+          character: 'あとで',
+          meanings: 'later on',
+        },
+        {
+          character: 'すぐ',
+          meanings: 'right away',
+        },
+        {
+          character: 'ゆっくり',
+          meanings: 'slowly; leisurely; unhurriedly',
+        },
+        {
+          character: 'けっこうです',
+          meanings: "That would be fine.; That wouldn't be necessary.",
+        },
+        {
+          character: 'ほんとうですか',
+          meanings: 'Really?',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_6_data;
