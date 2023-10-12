3be2cbdea916fa3ca44c424b128e56b0477da152
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-10-12 15:53:57 -0400

    Added lesson 7 vocabulary exercise

--- client/src/data/lessons/index.js
+++ client/src/data/lessons/index.js
@@ -5,7 +5,8 @@ import lesson_3_data from './lesson-3/lesson-3-data';
 import lesson_4_data from './lesson-4/lesson-4-data';
 import lesson_5_data from './lesson-5/lesson-5-data';
 import lesson_6_data from './lesson-6/lesson-6-data';
+import lesson_7_data from './lesson-7/lesson-7-data';
 
-const lessonData = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data, lesson_4_data, lesson_5_data, lesson_6_data];
+const lessonData = [lesson_0_data, lesson_1_data, lesson_2_data, lesson_3_data, lesson_4_data, lesson_5_data, lesson_6_data, lesson_7_data];
 
 export default lessonData;
---
+++ client/src/data/lessons/lesson-7/lesson-7-data.js
@@ -0,0 +1,240 @@
+const lesson_7_data = {
+  lessonNumber: 7,
+  lessonTitle: 'Family Picture',
+  lessonUrl: 'lesson-7',
+  lessonPages: 'Genki I: p.166-185',
+  lessonUnits: [
+    {
+      unitTitle: 'Vocabulary',
+      unitUrl: 'vocabulary',
+      unitContent: [
+        {
+          character: 'かぞく',
+          meanings: 'family',
+        },
+        {
+          character: 'おじいさん',
+          meanings: 'grandfather; old man',
+        },
+        {
+          character: 'おばあさん',
+          meanings: 'grandmother; old woman',
+        },
+        {
+          character: 'おにいさん',
+          meanings: 'older brother',
+        },
+        {
+          character: 'おねえさん',
+          meanings: 'older sister',
+        },
+        {
+          character: 'ちち',
+          meanings: '(my) father',
+        },
+        {
+          character: 'はは',
+          meanings: '(my) mother',
+        },
+        {
+          character: 'あに',
+          meanings: '(my) older brother',
+        },
+        {
+          character: 'あね',
+          meanings: '(my) older sister',
+        },
+        {
+          character: 'いもうと',
+          meanings: 'younger sister',
+        },
+        {
+          character: 'おとうと',
+          meanings: 'younger brother',
+        },
+        {
+          character: 'きょうだい',
+          meanings: 'brothers and sisters',
+        },
+        {
+          character: 'おとこのひと',
+          meanings: 'man',
+        },
+        {
+          character: 'おんなのひと',
+          meanings: 'woman',
+        },
+        {
+          character: 'かいしゃ',
+          meanings: 'company',
+        },
+        {
+          character: 'しょくどう',
+          meanings: 'cafeteria; dining commons',
+        },
+        {
+          character: 'デパート',
+          meanings: 'department store',
+        },
+        {
+          character: 'かみ',
+          meanings: 'hair',
+        },
+        {
+          character: 'くち',
+          meanings: 'mouth',
+        },
+        {
+          character: 'め',
+          meanings: 'eye',
+        },
+        {
+          character: 'めがね',
+          meanings: 'glasses',
+        },
+        {
+          character: 'うた',
+          meanings: 'song',
+        },
+        {
+          character: 'サークル',
+          meanings: 'club activity',
+        },
+        {
+          character: 'くるま',
+          meanings: 'car',
+        },
+        {
+          character: 'ながい',
+          meanings: 'long',
+        },
+        {
+          character: 'みじかい',
+          meanings: 'short (length)',
+        },
+        {
+          character: 'はやい',
+          meanings: 'fast',
+        },
+        {
+          character: 'せがたかい',
+          meanings: 'tall (stature)',
+        },
+        {
+          character: 'せがひくい',
+          meanings: 'short (stature)',
+        },
+        {
+          character: 'あたまがいい',
+          meanings: 'bright; smart; clever',
+        },
+        {
+          character: 'かわいい',
+          meanings: 'cute',
+        },
+        {
+          character: 'しんせつ（な）',
+          meanings: 'kind',
+        },
+        {
+          character: 'べんり（な）',
+          meanings: 'convenient',
+        },
+        {
+          character: 'うたう',
+          meanings: 'to sing',
+        },
+        {
+          character: 'かぶる',
+          meanings: 'to put on (a hat)',
+        },
+        {
+          character: 'はく',
+          meanings: 'to put on (items below your waist)',
+        },
+        {
+          character: 'しる',
+          meanings: 'to get to know',
+        },
+        {
+          character: 'しっています',
+          meanings: 'I know',
+        },
+        {
+          character: 'しりません',
+          meanings: 'I do not know',
+        },
+        {
+          character: 'すむ',
+          meanings: 'to live',
+        },
+        {
+          character: 'はたらく',
+          meanings: 'to work',
+        },
+        {
+          character: 'ふとる',
+          meanings: 'to gain weight',
+        },
+        {
+          character: 'ふとっています',
+          meanings: 'to be on the heavy side; overweight',
+        },
+        {
+          character: '（めがねを）かける',
+          meanings: 'to put on (glasses)',
+        },
+        {
+          character: 'きる',
+          meanings: 'to put on (clothes above your waist)',
+        },
+        {
+          character: 'やせる',
+          meanings: 'to lose weight',
+        },
+        {
+          character: 'やせています',
+          meanings: 'to be thin',
+        },
+        {
+          character: 'けっこんする',
+          meanings: 'to get married',
+        },
+        {
+          character: '～が',
+          meanings: '... , but',
+        },
+        {
+          character: 'なにも + negative',
+          meanings: 'not ... anything',
+        },
+        {
+          character: '～にん',
+          meanings: '(counter for people)',
+        },
+        {
+          character: 'ひとり',
+          meanings: 'one person',
+        },
+        {
+          character: 'ふたり',
+          meanings: 'two people',
+        },
+        {
+          character: 'べつに + negative',
+          meanings: 'nothing in particular',
+        },
+        {
+          character: 'もちろん',
+          meanings: 'of course',
+        },
+        {
+          character: 'よかったら',
+          meanings: 'if you like',
+        },
+      ],
+    },
+  ],
+};
+
+export default lesson_7_data;
