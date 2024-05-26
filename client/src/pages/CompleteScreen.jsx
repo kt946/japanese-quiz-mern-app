a6b005e647accb441dfa348f7f30d8448089acf4
KT 2022 <103476893+kt946@users.noreply.github.com>
2024-05-26 16:07:53 -0400

    Add sounds to quiz and complete pages

--- client/src/pages/CompleteScreen.jsx
+++ client/src/pages/CompleteScreen.jsx
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
+import { useAudio } from 'react-use';
 import { easeInOut, easeOut, stagger, useAnimate } from 'framer-motion';
 import { runFireworks } from '../utils/confetti';
 
 const CompleteScreen = ({ quiz }) => {
   // This uses the useAnimate hook from Framer Motion to create a new animation scope.
   const [scope, animate] = useAnimate();
+
+  // Get the url of the origin
+  const path = window.location.origin;
+
+  // This hook is used to play the finish audio when the component is mounted.
+  const [finishAudio] = useAudio({ src: `${path}/src/assets/sounds/finish.mp3`, autoPlay: true });
 
   // This useEffect hook will run once the component is mounted.
   // It is used to control the animations.
@@ -16,7 +23,11 @@ const CompleteScreen = ({ quiz }) => {
       runFireworks();
       await animate('#complete-title', { y: 0 }, { duration: 0.4, delay: 1, ease: easeInOut, type: 'tween' });
       await animate('#divider', { opacity: 1, width: '100%' }, { delay: 0.2, ease: easeInOut });
-      await animate('.quiz-stat-container',{ opacity: 1, y: 0 },{ duration: 0.5, delay: stagger(0.3), ease: easeOut });
+      await animate(
+        '.quiz-stat-container',
+        { opacity: 1, y: 0 },
+        { duration: 0.5, delay: stagger(0.3), ease: easeOut }
+      );
     };
 
     playAnimations();
@@ -28,70 +39,73 @@ const CompleteScreen = ({ quiz }) => {
 
   return (
     // This div is the animation scope. It will be used to animate the entire component.
-    <div
-      ref={scope}
-      className="grow flex flex-col justify-center items-center text-center gap-8"
-    >
-      <h1
-        id="complete-title"
-        style={{ opacity: 0 }}
-        className="font-bold text-4xl md:text-5xl lg:text-6xl"
+    <>
+      {finishAudio}
+      <div
+        ref={scope}
+        className="grow flex flex-col justify-center items-center text-center gap-8"
       >
-        Quiz Completed!
-      </h1>
-      <hr
-        id="divider"
-        style={{ opacity: 0, width: 0 }}
-        className="w-full max-w-2xl border-4 border-primary"
-      />
-      {/* Quiz Statistics Cards */}
-      <div className="w-full max-w-2xl flex flex-row gap-2 sm:gap-4 md:gap-8 font-bold">
-        {/* Score Card */}
-        <div
+        <h1
+          id="complete-title"
           style={{ opacity: 0 }}
-          className="quiz-stat-container bg-amber-400"
+          className="font-bold text-4xl md:text-5xl lg:text-6xl"
         >
-          {/* Card Header */}
-          <div className="p-1">
-            <h2 className="uppercase">Score</h2>
+          Quiz Completed!
+        </h1>
+        <hr
+          id="divider"
+          style={{ opacity: 0, width: 0 }}
+          className="w-full max-w-2xl border-4 border-primary"
+        />
+        {/* Quiz Statistics Cards */}
+        <div className="w-full max-w-2xl flex flex-row gap-2 sm:gap-4 md:gap-8 font-bold">
+          {/* Score Card */}
+          <div
+            style={{ opacity: 0 }}
+            className="quiz-stat-container bg-amber-400"
+          >
+            {/* Card Header */}
+            <div className="p-1">
+              <h2 className="uppercase">Score</h2>
+            </div>
+            {/* Card Body */}
+            <div className="quiz-stat-body">
+              <h3 className="text-amber-700 dark:text-amber-400">{score} %</h3>
+            </div>
           </div>
-          {/* Card Body */}
-          <div className="quiz-stat-body">
-            <h3 className="text-amber-700 dark:text-amber-400">{score} %</h3>
-          </div>
-        </div>
 
-        {/* XP Card */}
-        <div
-          style={{ opacity: 0 }}
-          className="quiz-stat-container bg-sky-400"
-        >
-          {/* Card Header */}
-          <div className="p-1">
-            <h2 className="uppercase">Earned</h2>
+          {/* XP Card */}
+          <div
+            style={{ opacity: 0 }}
+            className="quiz-stat-container bg-sky-400"
+          >
+            {/* Card Header */}
+            <div className="p-1">
+              <h2 className="uppercase">Earned</h2>
+            </div>
+            {/* Card Body */}
+            <div className="quiz-stat-body">
+              <h3 className="text-sky-700 dark:text-sky-400">+{xp} XP</h3>
+            </div>
           </div>
-          {/* Card Body */}
-          <div className="quiz-stat-body">
-            <h3 className="text-sky-700 dark:text-sky-400">+{xp} XP</h3>
-          </div>
-        </div>
 
-        {/* Time Card */}
-        <div
-          style={{ opacity: 0 }}
-          className="quiz-stat-container bg-lime-400"
-        >
-          {/* Card Header */}
-          <div className="p-1">
-            <h2 className="uppercase">Time</h2>
-          </div>
-          {/* Card Body */}
-          <div className="quiz-stat-body">
-            <h3 className="text-lime-700 dark:text-lime-400">{time}</h3>
+          {/* Time Card */}
+          <div
+            style={{ opacity: 0 }}
+            className="quiz-stat-container bg-lime-400"
+          >
+            {/* Card Header */}
+            <div className="p-1">
+              <h2 className="uppercase">Time</h2>
+            </div>
+            {/* Card Body */}
+            <div className="quiz-stat-body">
+              <h3 className="text-lime-700 dark:text-lime-400">{time}</h3>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- client/src/pages/QuizPage.jsx
+++ client/src/pages/QuizPage.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import { Navigate } from 'react-router-dom';
+import { useAudio } from 'react-use';
 import Auth from '../utils/auth';
 import { HiX } from 'react-icons/hi';
 import { AiOutlineLoading } from 'react-icons/ai';
@@ -14,6 +15,13 @@ import { QUERY_ME } from '../utils/queries';
 const QuizPage = ({ quiz }) => {
   if (!Auth.loggedIn()) return <Navigate to="/login" />;
 
+  // Get the url of the origin
+  const path = window.location.origin;
+
+  // This hook is used to play the correct and incorrect audio when the user answers a question.
+  const [correctAudio, _c, correctControls] = useAudio({ src: `${path}/src/assets/sounds/correct.wav` });
+  const [incorrectAudio, _i, incorrectControls] = useAudio({ src: `${path}/src/assets/sounds/incorrect.wav` });
+
   const [selectedOption, setSelectedOption] = useState(null);
   const [questionState, setQuestionState] = useState(null);
   const [question, setQuestion] = useState(quiz.generateQuestion());
@@ -23,10 +31,12 @@ const QuizPage = ({ quiz }) => {
   // check answer and update progress
   const checkAnswer = (answer) => {
     if (answer === question.answer) {
+      correctControls.play();
       setQuestionState('correct');
       quiz.incrementNumCorrect();
       quiz.incrementProgress();
     } else {
+      incorrectControls.play();
       setQuestionState('incorrect');
       quiz.incrementNumIncorrect();
       quiz.decrementProgress();
@@ -77,161 +87,168 @@ const QuizPage = ({ quiz }) => {
   };
 
   return (
-    <div
-      id="characters"
-      className="w-full h-screen max-h-screen p-4 py-6 md:p-0 flex flex-col"
-    >
-      {/* Quiz Header */}
-      {!quizComplete && (
-        <div className="md:h-20">
-          <div className="w-full h-full max-w-5xl mx-auto md:pt-12 md:px-4 flex items-center">
-            {/* Back Button */}
-            <button
-              type="button"
-              onClick={() => window.history.back()}
-              className="hover:opacity-60 mr-4"
-            >
-              <HiX className="w-7 h-7" />
-            </button>
-
-            {/* Progress Bar */}
-            <div className="bg-gray-300 dark:bg-gray-700 h-4 w-full rounded-2xl overflow-x-hidden">
-              {/* Outer Bar */}
-              <div
-                className={`${
-                  progress <= 0 ? 'opacity-0' : ''
-                } custom-transition h-full px-2 pt-1 bg-gradient-to-b from-primary-tint to-red-800 rounded-2xl`}
-                style={{ width: `${progress}%` }}
+    <>
+      {correctAudio}
+      {incorrectAudio}
+      <div
+        id="characters"
+        className="w-full h-screen max-h-screen p-4 py-6 md:p-0 flex flex-col"
+      >
+        {/* Quiz Header */}
+        {!quizComplete && (
+          <div className="md:h-20">
+            <div className="w-full h-full max-w-5xl mx-auto md:pt-12 md:px-4 flex items-center">
+              {/* Back Button */}
+              <button
+                type="button"
+                onClick={() => window.history.back()}
+                className="hover:opacity-60 mr-4"
               >
-                {/* Inner Bar */}
-                <div className="bg-white/30 h-1 rounded-2xl" />
+                <HiX className="w-7 h-7" />
+              </button>
+
+              {/* Progress Bar */}
+              <div className="bg-gray-300 dark:bg-gray-700 h-4 w-full rounded-2xl overflow-x-hidden">
+                {/* Outer Bar */}
+                <div
+                  className={`${
+                    progress <= 0 ? 'opacity-0' : ''
+                  } custom-transition h-full px-2 pt-1 bg-gradient-to-b from-primary-tint to-red-800 rounded-2xl`}
+                  style={{ width: `${progress}%` }}
+                >
+                  {/* Inner Bar */}
+                  <div className="bg-white/30 h-1 rounded-2xl" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
 
-      {/* Quiz Main */}
-      {!quizComplete && (
-        <div className="w-full h-full my-2 flex flex-col md:grid justify-center items-center md:content-center">
-          <div className="w-full max-w-2xl md:w-[600px] h-full md:min-h-[450px] quiz-main-container gap-2 md:gap-6">
-            <h1 className="font-bold text-xl sm:text-2xl md:text-3xl">
-              <span>{question.questionDirection}</span>{' '}
-              <span>"{question.questionSubject}"</span>
-            </h1>
-            <div className="font-medium text-2xl sm:text-3xl md:text-4xl grid grid-cols-1 gap-2">
-              {question.choices.map((choice) => (
-                <button
-                  key={`id-${choice}`}
-                  type="button"
-                  className={`w-full h-full p-2 md:py-3 rounded-xl border-2 ${
-                    selectedOption === choice
-                      ? 'bg-sky-200 border-2 border-sky-400 dark:bg-sky-700'
-                      : `border-gray-300 dark:border-gray-700 ${
-                          !questionState && 'hover:bg-gray-200 dark:hover:bg-slate-800'
-                        }`
-                  }`}
-                  onClick={() => setSelectedOption(choice)}
-                  disabled={questionState}
-                >
-                  <div className="flex flex-col grow w-full">
-                    <span className="inline-flex justify-center items-center grow">{choice}</span>
-                  </div>
-                </button>
-              ))}
+        {/* Quiz Main */}
+        {!quizComplete && (
+          <div className="w-full h-full my-2 flex flex-col md:grid justify-center items-center md:content-center">
+            <div className="w-full max-w-2xl md:w-[600px] h-full md:min-h-[450px] quiz-main-container gap-2 md:gap-6">
+              <h1 className="font-bold text-xl sm:text-2xl md:text-3xl">
+                <span>{question.questionDirection}</span> <span>"{question.questionSubject}"</span>
+              </h1>
+              <div className="font-medium text-2xl sm:text-3xl md:text-4xl grid grid-cols-1 gap-2">
+                {question.choices.map((choice) => (
+                  <button
+                    key={`id-${choice}`}
+                    type="button"
+                    className={`w-full h-full p-2 md:py-3 rounded-xl border-2 ${
+                      selectedOption === choice
+                        ? 'bg-sky-200 border-2 border-sky-400 dark:bg-sky-700'
+                        : `border-gray-300 dark:border-gray-700 ${
+                            !questionState && 'hover:bg-gray-200 dark:hover:bg-slate-800'
+                          }`
+                    }`}
+                    onClick={() => setSelectedOption(choice)}
+                    disabled={questionState}
+                  >
+                    <div className="flex flex-col grow w-full">
+                      <span className="inline-flex justify-center items-center grow">{choice}</span>
+                    </div>
+                  </button>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
 
-      {/* Quiz Completion Screen */}
-      {quizComplete && <CompleteScreen quiz={quiz} />}
+        {/* Quiz Completion Screen */}
+        {quizComplete && <CompleteScreen quiz={quiz} />}
 
-      {/* Quiz Footer */}
-      <div
-        className={`-mx-4 -mb-6 mt-4 max-md:pb-6 md:m-0 md:h-36 md:min-h-[144px] md:border-t-2 ${
-          questionState === 'correct'
-            ? 'border-[#CEFEA8] bg-[#CEFEA8] dark:bg-slate-800 dark:border-gray-700'
-            : questionState === 'incorrect'
-            ? 'border-[#FED6DD] bg-[#FED6DD] dark:bg-slate-800 dark:border-gray-700'
-            : 'border-gray-300 dark:border-gray-700'
-        }`}
-      >
-        <div className="w-full h-full max-w-5xl mx-auto px-4 flex items-center">
-          {/* Footer Buttons */}
-          {!quizComplete && (
-            <div className="w-full flex flex-col md:flex-row justify-between md:items-center">
-              {!questionState ? (
-                // Skip Button
-                <Button
-                  type="button"
-                  btnStyle="hidden md:flex justify-center items-center gap-2 text-sky-500 border-2 border-sky-500 bg-transparent hover:bg-gray-200 dark:hover:bg-slate-800"
-                  onClick={() => checkAnswer('skip')}
-                  title="Skip"
-                />
-              ) : (
-                // Feedback Message
-                <FeedbackMessage
-                  questionState={questionState}
-                  answer={question.answer}
-                />
-              )}
+        {/* Quiz Footer */}
+        <div
+          className={`-mx-4 -mb-6 mt-4 max-md:pb-6 md:m-0 md:h-36 md:min-h-[144px] md:border-t-2 ${
+            questionState === 'correct'
+              ? 'border-[#CEFEA8] bg-[#CEFEA8] dark:bg-slate-800 dark:border-gray-700'
+              : questionState === 'incorrect'
+              ? 'border-[#FED6DD] bg-[#FED6DD] dark:bg-slate-800 dark:border-gray-700'
+              : 'border-gray-300 dark:border-gray-700'
+          }`}
+        >
+          <div className="w-full h-full max-w-5xl mx-auto px-4 flex items-center">
+            {/* Footer Buttons */}
+            {!quizComplete && (
+              <div className="w-full flex flex-col md:flex-row justify-between md:items-center">
+                {!questionState ? (
+                  // Skip Button
+                  <Button
+                    type="button"
+                    btnStyle="hidden md:flex justify-center items-center gap-2 text-sky-500 border-2 border-sky-500 bg-transparent hover:bg-gray-200 dark:hover:bg-slate-800"
+                    onClick={() => checkAnswer('skip')}
+                    title="Skip"
+                  />
+                ) : (
+                  // Feedback Message
+                  <FeedbackMessage
+                    questionState={questionState}
+                    answer={question.answer}
+                  />
+                )}
 
-              {/* Check and Next Buttons */}
-              {!questionState ? (
-                // Check Button
-                <Button
-                  type="button"
-                  btnStyle={`flex justify-center items-center gap-2
+                {/* Check and Next Buttons */}
+                {!questionState ? (
+                  // Check Button
+                  <Button
+                    type="button"
+                    btnStyle={`flex justify-center items-center gap-2
                    ${
                      !selectedOption
                        ? 'text-gray-500 bg-gray-300'
                        : 'text-white dark:text-slate-800 bg-[#58CC02] dark:bg-lime-500 hover:bg-[#4CAD02] dark:hover:bg-lime-600'
                    }
                       `}
-                  onClick={() => checkAnswer(selectedOption)}
-                  disabled={!selectedOption}
-                  title="Check"
-                />
-              ) : (
-                // Next Button
+                    onClick={() => checkAnswer(selectedOption)}
+                    disabled={!selectedOption}
+                    title="Check"
+                  />
+                ) : (
+                  // Next Button
+                  <Button
+                    type="button"
+                    btnStyle={`flex justify-center items-center gap-2 text-white dark:text-slate-800 ${
+                      questionState === 'correct'
+                        ? 'bg-[#58CC02] dark:bg-lime-500 hover:bg-[#4CAD02] dark:hover:bg-lime-600'
+                        : 'bg-red-600 dark:bg-red-400 hover:bg-red-700 dark:hover:bg-red-500'
+                    }`}
+                    onClick={() => cycleNextQuestion()}
+                    title={!loading && 'Next'}
+                    icon={
+                      loading && (
+                        <AiOutlineLoading className="text-white dark:text-slate-800 animate-spin h-6 w-6 mx-auto" />
+                      )
+                    }
+                  />
+                )}
+              </div>
+            )}
+
+            {quizComplete && (
+              <div className="w-full flex justify-between gap-2">
+                {/* Try Again Button */}
                 <Button
                   type="button"
-                  btnStyle={`flex justify-center items-center gap-2 text-white dark:text-slate-800 ${
-                    questionState === 'correct'
-                      ? 'bg-[#58CC02] dark:bg-lime-500 hover:bg-[#4CAD02] dark:hover:bg-lime-600'
-                      : 'bg-red-600 dark:bg-red-400 hover:bg-red-700 dark:hover:bg-red-500'
-                  }`}
-                  onClick={() => cycleNextQuestion()}
-                  title={!loading && 'Next'}
-                  icon={loading && <AiOutlineLoading className="text-white dark:text-slate-800 animate-spin h-6 w-6 mx-auto" />}
+                  btnStyle="flex justify-center items-center gap-2 text-sky-500 border-2 border-sky-500 bg-transparent hover:bg-gray-200 dark:hover:bg-slate-800"
+                  onClick={() => window.location.reload()}
+                  title="Try Again"
                 />
-              )}
-            </div>
-          )}
 
-          {quizComplete && (
-            <div className="w-full flex justify-between gap-2">
-              {/* Try Again Button */}
-              <Button
-                type="button"
-                btnStyle="flex justify-center items-center gap-2 text-sky-500 border-2 border-sky-500 bg-transparent hover:bg-gray-200 dark:hover:bg-slate-800"
-                onClick={() => window.location.reload()}
-                title="Try Again"
-              />
-
-              {/* Continue Button */}
-              <Button
-                type="button"
-                btnStyle="flex justify-center items-center gap-2 text-white dark:text-slate-800 bg-[#58CC02] hover:bg-[#4CAD02]"
-                onClick={() => history.back()}
-                title="Continue"
-              />
-            </div>
-          )}
+                {/* Continue Button */}
+                <Button
+                  type="button"
+                  btnStyle="flex justify-center items-center gap-2 text-white dark:text-slate-800 bg-[#58CC02] hover:bg-[#4CAD02]"
+                  onClick={() => history.back()}
+                  title="Continue"
+                />
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
